682e1ca94b8e6d95249a4b6e51c4255224e604fd
R Devon Brewster <devonbrewster42@gmail.com>
2022-10-09 13:35:53 -0500

    begun updating game for new version

--- src/components/welcome/PlayerForm.js
+++ src/components/welcome/PlayerForm.js
@@ -5,70 +5,54 @@ import DataService from '../../service/DataService';
 
 const PlayerForm = props => {
     const [name, setName] = useState("Mr. Magoo");
-    const [names] = useState([])
-    const [player, setPlayer] = useState(1);
-    let [isChecked, setIsChecked] = useState(true);
-    let [bothChecked, setBothChecked] = useState(true);
+    const [email, setEmail] = useState("Magoo@gmail.com");
     const history = useHistory();
 
-    const handleChange = e => {               
+    const handleName = e => {               
         setName(e.target.value);
     }   
+    const handleEmail = e => {               
+        setEmail(e.target.value);
+    }   
 
-    const handleUndo = e => {        
-        console.log(e.target);
-        setIsChecked((prev) => !prev);
-    }
-
-    const addPlayers = body => {        
-        console.log(body)
+    const makePlayer = (e) => {
+        e.preventDefault(); 
+        console.log(e.target.name.value);
+        const body = {                
+            name, 
+            email
+        }
+        // const body = {                
+        //     name : e.target.name.value, 
+        //     email : e.target.email.value
+        // }
+       
         DataService.createPlayers(body)
         .then(res => {
-            if (bothChecked){
-                console.log(bothChecked, "both")
-                props.toggleUndo(false);
-            } else {
-                props.toggleUndo(true);
-            }
-            console.log("added player", res);
-            let gameId = res.data[64].id;
-            props.setTheBoard(res.data);  
-            console.log("the game id is ", gameId);        
-            history.push(`/game/${gameId}`);
+            console.log(res)
+        //     if (bothChecked){
+        //         console.log(bothChecked, "both")
+        //         props.toggleUndo(false);
+        //     } else {
+        //         props.toggleUndo(true);
+        //     }
+        //     console.log("added player", res);
+        //     let gameId = res.data[64].id;
+        //     props.setTheBoard(res.data);  
+        //     console.log("the game id is ", gameId);        
+        //     history.push(`/game/${gameId}`);
         })
         .catch(err => {
             console.log(err)
         })
     }
-
-    const makePlayer = (e) => {
-        e.preventDefault(); 
-        console.log(e.target.name.value);
-        console.log(isChecked);
-        names.push(e.target.name.value);  
-        if (!isChecked){
-            console.log("one unchecked");
-            setBothChecked(false);
-        }      
-        if (player === 2){
-            const body = {                
-                name1: names[0], 
-                name2: names[1]
-            }
-            addPlayers(body);
-            setPlayer(1);                   
-        }
-        setName("");
-        setPlayer(2);
-    }
     
 
     return ( 
         <form onSubmit={makePlayer}>
-            Player {player}, Please Enter Your Name <br></br>
-            <input type="text" name="name" onChange={handleChange} value={name} /><br></br>
-            <label>Include an Undo option</label><input type="checkbox" name="undo" onChange={handleUndo} checked={isChecked} />
-            <br></br>
+            Please Enter Your Name and a Valid Email <br></br>
+            <input type="text" name="name" onChange={handleName} value={name} /><br></br>
+            <input type="text" name="email" onChange={handleEmail} value={email} /><br></br>
             <input type="submit" value="Submit" />
         </form>
      );
--- src/service/DataService.js
+++ src/service/DataService.js
@@ -5,6 +5,24 @@ const url = 'http://localhost:8080/game';
 
 class DataService {    
 
+    createUser(body) {
+        return axios.post(`${url}/user`, body)
+    }
+    startLocalGame(body) {
+        return axios.post(`${url}`, body)
+    }
+    selectPiece(body, id) {
+        return axios.get(`${url}/${id}`, body);
+    }
+    movePiece(body, id) {
+        return axios.post(`${url}/${id}`, body);
+    }
+    selectPromotion(body, id) {
+        return axios.post(`${url}/${id}/promotion`, body);
+    }
+
+
+
     restartGame(){
         return axios.post(`${url}/restart`);
     }
