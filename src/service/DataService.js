1a581feb3acd71783c0d9cc3a3658ea866a7ce02
R Devon Brewster <devonbrewster42@gmail.com>
2023-08-06 15:36:35 -0500

    updated dataService for talkign to backend

--- src/service/DataService.js
+++ src/service/DataService.js
@@ -11,28 +11,35 @@ class DataService {
     login(body) {
         return axios.get(`${url}/user`, body)
     }
+
+
     startLocalGame(body) {
-        return axios.post(`${url}/game`, body)
+        return axios.post(`${url}/chess`, body)
+    }
+    restartGame(body) {
+        return axios.post(`${url}/chess/restart`, body)
+    }
+    rejoinGame(body) {
+        return axios.get(`${url}/chess/rejoin`, body)
+    }
+    rejoinGameById(id) {
+        return axios.get(`${url}/chess/rejoin/${id}`)
+    }
+
+    movePiece(body, id) {
+        return axios.post(`${url}/chess/${id}`, body);
     }
     getAllMoves(body, id) {
-        return axios.get(`${url}/game/${id}`, body);
-    }
-    movePiece(body, id) {
-        return axios.post(`${url}/game/${id}`, body);
-    }
-
-    restartGame(body) {
-        return axios.post(`${url}/game/restart`, body)
+        return axios.get(`${url}/chess/${id}`, body);
     }
     requestDraw(id) {
-        return axios.post(`${url}/game/${id}/draw`);
+        return axios.post(`${url}/chess/${id}/draw`);
     }
     forfeit(id) {
-        return axios.post(`${url}/game/${id}/forfeit`);
+        return axios.post(`${url}/chess/${id}/forfeit`);
     }
-
     undo(id){
-        return axios.post(`${url}/game/${id}/undo`);
+        return axios.post(`${url}/chess/${id}/undo`);
     }
    
 }
